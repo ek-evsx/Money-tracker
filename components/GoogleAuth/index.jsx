156a9f7ec0145b1648db7e3ef25f74512416d4a2
Yauheni Kaliada <y.kaliada@godeltech.com>
2021-12-20 16:00:27 +0300

    Demo

---
+++ components/GoogleAuth/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+
+export const GoogleAuth = props => {
+    retutn (
+        <GoogleLogin
+            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+        />
+    );
+};
