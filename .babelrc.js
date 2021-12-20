3d4898145d40bd70cf8120daa15e70b3bb808e29
Yauheni Kaliada <y.kaliada@godeltech.com>
2021-12-20 16:36:47 +0300

    #Merged MT-2_add_linter

--- .babelrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    presets: [['next/babel']],
-    plugins: [['import', { libraryName: 'antd', style: true }]],
-    loader: 'less-loader', options: { javascriptEnabled: true },
-};
---
+++ pages/_app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import '../styles/variables.less';
+
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
