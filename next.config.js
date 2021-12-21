823bafadeaa183e23c86923cdccc7e22791d29e2
Evgeniy Koleda <43078734+EvgeniyKoleda@users.noreply.github.com>
2021-12-21 13:10:38 +0300

    Mt 4 fix build (#1)
    * #MT-4 Add prettier to pre commit
    
    * #MT-4 Fix prettier
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+// eslint-disable-next-line
 const withAntdLess = require('next-plugin-antd-less');
 
+// eslint-disable-next-line
 module.exports = withAntdLess({
   lessVarsFilePath: './styles/variables.less',
   lessVarsFilePathAppendToEndOfContent: false,
   cssLoaderOptions: {
-    loader: 'less-loader', options: { javascriptEnabled: true },
+    loader: 'less-loader',
+    options: { javascriptEnabled: true },
   },
   webpack(config) {
     return config;
