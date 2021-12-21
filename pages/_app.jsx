adb5c1fe1422918afa45a6e9a2124616f8c86211
Evgeniy Koleda <43078734+EvgeniyKoleda@users.noreply.github.com>
2021-12-21 14:51:55 +0300

    #MT-7 Setup config (#4)
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { initializeApp } from 'firebase/app';
 
+import { FIREBASE } from '../utils/config';
+
 import '../styles/variables.less';
 
 const MyApp = ({ Component, pageProps }) => {
-  const firebaseConfig = {
-    apiKey: 'AIzaSyBJoBvblW3VnIZ0vjK-6vF6PfQaZ6WCApc',
-    authDomain: 'money-tracker-f2c6a.firebaseapp.com',
-    projectId: 'money-tracker-f2c6a',
-    storageBucket: 'money-tracker-f2c6a.appspot.com',
-    messagingSenderId: '500663819238',
-    appId: '1:500663819238:web:5223ec9299251bd77fd427',
-    measurementId: 'G-LPVCJY39F0',
-  };
-
   // Initialize Firebase
-  initializeApp(firebaseConfig);
+  initializeApp(FIREBASE);
 
   const getLayout = Component.getLayout || ((page) => page);
 
