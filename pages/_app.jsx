de9490ce260c147911e79c28f04529c3f751284e
Evgeniy Koleda <43078734+EvgeniyKoleda@users.noreply.github.com>
2021-12-22 10:25:54 +0300

    #MT-9 Add context (#7)
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { initializeApp } from 'firebase/app';
 
+import { useAuth } from '../hooks/useAuth';
+
 import { FIREBASE } from '../utils/config';
+import { DELAY } from '../utils/constants';
+
+import { Spin } from '../components/Spin';
 
 import '../styles/variables.less';
+
+export const AppContext = React.createContext({});
 
 const MyApp = ({ Component, pageProps }) => {
   // Initialize Firebase
   initializeApp(FIREBASE);
 
+  const [isLoading, user] = useAuth({ delay: DELAY });
+
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <AppContext.Provider value={{ user }}>
+      {isLoading && <Spin isFullScreen={true} size='large' />}
+      <AppContext.Consumer>
+        {(value) => getLayout(<Component {...pageProps} />, value)}
+      </AppContext.Consumer>
+    </AppContext.Provider>
+  );
 };
 
 export default MyApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
 
-import { useAuth } from '../hooks/useAuth';
-
 import getLayout from '../utils/getLayout';
-import { DELAY } from '../utils/constants';
-
-import { Spin } from '../components/Spin';
 
 export default function Home() {
-  const isLoading = useAuth({ delay: DELAY });
-
-  return (
-    <React.Fragment>
-      {isLoading && <Spin isFullScreen={true} size='large' />}
-    </React.Fragment>
-  );
+  return <React.Fragment></React.Fragment>;
 }
 
 Home.getLayout = getLayout({ title: 'Home' });
