dc194becdd65b8a094befb88be668a4864de43a4
Evgeniy Koleda <43078734+EvgeniyKoleda@users.noreply.github.com>
2021-12-23 16:25:05 +0300

    Develop (#17)
    * Fix build after merge (#10)
    
    * Develop to master (#5)
    
    Develop To master
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    * Fix build after merge
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    * Master (#12)
    
    * Develop to master (#5)
    
    Develop To master
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    * Fix build after merge (#10) (#11)
    
    * Develop to master (#5)
    
    Develop To master
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    * Fix build after merge
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    * Fix conflicts after merge
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    * #MT-11 Fix title color (#13)
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    * #MT-12 Add deleting wallet (#15)
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    * Mt 13 add base wallet page (#16)
    
    * #MT-13 Refactor dashboard
    
    * #MT-13 Add routing to wallet page
    
    * #MT-13 Add base wallet page
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>
    
    Co-authored-by: Yauheni Kaliada <y.kaliada@godeltech.com>

---
+++ components/WalletCard/index.jsx
@@ -0,0 +1,136 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Row, Col, Card, Collapse, Empty, Button, Modal } from 'antd';
+import { getDatabase, ref, onValue, remove } from 'firebase/database';
+import { DeleteOutlined } from '@ant-design/icons';
+import Link from 'next/link';
+
+import { AppContext } from '../../pages/_app';
+
+import { CreateWallet } from '../CreateWallet';
+
+import styles from './WalletCard.module.less';
+
+const getUserWalletsPath = (userId) => 'users/' + userId + '/wallets';
+
+const CardTitle = (props) => {
+  return (
+    <div className={styles.cardHeadTitleWrapper}>
+      <span className={styles.cardHeadTitle}>{props.title}</span>
+      <CreateWallet />
+    </div>
+  );
+};
+
+const WalletHeader = (props) => {
+  return (
+    <div className={styles.walletInfo}>
+      <span className={styles.walletCurrency}>{props.wallet.currency}</span>
+      <span className={styles.walletName}>{props.wallet.name}</span>{' '}
+      <span
+        className={`${styles.walletAmount} ${
+          props.wallet.amount < 0 && styles.walletAmountNegative
+        }`}
+      >
+        {props.wallet.amount}
+      </span>
+    </div>
+  );
+};
+
+export const WalletCard = () => {
+  const appContext = useContext(AppContext);
+  const [wallets, setWallets] = useState([]);
+  const [walletToDelete, setWalletToDelete] = useState(null);
+
+  useEffect(() => {
+    if (appContext?.user?.uid) {
+      const db = getDatabase();
+      const userWalletsRef = ref(db, getUserWalletsPath(appContext?.user?.uid));
+
+      onValue(userWalletsRef, (snapshot) => {
+        const fetchedWallets = [];
+        const values = snapshot.val();
+
+        Object.keys(values).forEach((key) =>
+          fetchedWallets.push({
+            id: key,
+            ...values[key],
+          })
+        );
+
+        setWallets(fetchedWallets);
+      });
+    }
+  }, [appContext?.user?.uid]);
+
+  const onConfirmDelete = () => {
+    const db = getDatabase();
+    const userWalletsRef = ref(
+      db,
+      `${getUserWalletsPath(appContext?.user?.uid)}/${walletToDelete.id}`
+    );
+
+    remove(userWalletsRef).then(setWalletToDelete(null)).catch(console.error);
+
+    setWalletToDelete(null);
+  };
+
+  const onCancelDelete = () => setWalletToDelete(null);
+
+  const onDelete = (wallet) => () => setWalletToDelete(wallet);
+
+  return (
+    <Card
+      className={styles.walletCard}
+      hoverable
+      title={<CardTitle title='Wallets' />}
+    >
+      {wallets?.length ? (
+        <Collapse>
+          {wallets?.map((wallet, idx) => (
+            <Collapse.Panel header={<WalletHeader wallet={wallet} />} key={idx}>
+              <Row>
+                <Col span={19}>Wallet Description</Col>
+                <Col span={5}>
+                  <Row justify='space-between'>
+                    <Link
+                      href={`/wallets/${encodeURIComponent(wallet.id)}`}
+                      passHref
+                    >
+                      <Button type='primary' ghost>
+                        Details
+                      </Button>
+                    </Link>
+
+                    <Button
+                      icon={<DeleteOutlined />}
+                      onClick={onDelete(wallet)}
+                      danger
+                      type='primary'
+                    >
+                      Delete
+                    </Button>
+                    <Modal
+                      visible={walletToDelete}
+                      title='Are you shure?'
+                      onCancel={onCancelDelete}
+                      onOk={onConfirmDelete}
+                    >
+                      <p>
+                        Your wallet &quot;{walletToDelete?.name}&quot; would be
+                        deleted!
+                      </p>
+                      <p>Please confirm or decline your action.</p>
+                    </Modal>
+                  </Row>
+                </Col>
+              </Row>
+            </Collapse.Panel>
+          ))}
+        </Collapse>
+      ) : (
+        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+      )}
+    </Card>
+  );
+};
--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,86 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Row, Col, Card, Collapse, Empty } from 'antd';
-import { getDatabase, ref, onValue } from 'firebase/database';
+import React from 'react';
+import { Row, Col } from 'antd';
 
 import getLayout from '../utils/getLayout';
 
-import { AppContext } from '../pages/_app';
-
-import { CreateWallet } from '../components/CreateWallet';
+import { WalletCard } from '../components/WalletCard';
 
 import styles from '../styles/Dashboard.module.less';
 
-const getUserWalletsPath = (userId) => 'users/' + userId + '/wallets';
-
-const CardTitle = (props) => {
-  return (
-    <div className={styles.cardHeadTitleWrapper}>
-      <span className={styles.cardHeadTitle}>{props.title}</span>
-      <CreateWallet />
-    </div>
-  );
-};
-
-const WalletHeader = (props) => {
-  return (
-    <div className={styles.walletInfo}>
-      <span className={styles.walletCurrency}>{props.wallet.currency}</span>
-      <span className={styles.walletName}>{props.wallet.name}</span>{' '}
-      <span
-        className={`${styles.walletAmount} ${
-          props.wallet.amount < 0 && styles.walletAmountNegative
-        }`}
-      >
-        {props.wallet.amount}
-      </span>
-    </div>
-  );
-};
-
 export default function Dashboard() {
-  const appContext = useContext(AppContext);
-  const [wallets, setWallets] = useState([]);
-
-  useEffect(() => {
-    if (appContext?.user?.uid) {
-      const db = getDatabase();
-      const userWalletsRef = ref(db, getUserWalletsPath(appContext?.user?.uid));
-
-      onValue(userWalletsRef, (snapshot) => {
-        const fetchedWallets = [];
-        const values = snapshot.val();
-
-        Object.keys(values).forEach((key) => fetchedWallets.push(values[key]));
-
-        setWallets(fetchedWallets);
-      });
-    }
-  }, [appContext?.user?.uid]);
-
   return (
     <div className={styles.container}>
       <Row gutter={[16, 16]}>
         <Col span={12}>
-          <Card
-            className={styles.walletCard}
-            hoverable
-            title={<CardTitle title='Wallets' />}
-          >
-            {wallets?.length ? (
-              <Collapse>
-                {wallets?.map((wallet, idx) => (
-                  <Collapse.Panel
-                    header={<WalletHeader wallet={wallet} />}
-                    key={idx}
-                  >
-                    Wallet Description
-                  </Collapse.Panel>
-                ))}
-              </Collapse>
-            ) : (
-              <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-            )}
-          </Card>
+          <WalletCard />
         </Col>
       </Row>
     </div>
